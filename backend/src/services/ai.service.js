3b8f593b7c058896629c82af9293b76cc8de10b8
ABhay7398 <216280932+Abhay23043@users.noreply.github.com>
2026-09-14 14:42:22 +0530

    updating puppter

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -119,20 +119,14 @@ async function generateHtmlToPdf(htmlContent) {
 
     try {
 
-        const executablePath =await puppeteer.executablePath()
-
-
-
         browser = await puppeteer.launch({
             headless: true,
-            executablePath,
             args: [
                 "--no-sandbox",
                 "--disable-setuid-sandbox",
                 "--disable-dev-shm-usage"
             ]
         })
-
         const page = await browser.newPage()
 
         await page.setContent(htmlContent, {
